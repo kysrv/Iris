cbd8faf5ef79c265d6ae7a6125606e703fbd64ae
unknown <kyshu.senpai@gmail.com>
2021-07-04 22:21:41 +0200

    corrected app-config

--- Batman/src/app-config.js
+++ Batman/src/app-config.js
@@ -12,11 +12,12 @@ if (document.location.origin.includes("localhost")) {
 } else if (document.location.origin.includes("ngrok")) {
   // * si c'est forward sur ngrok l'api sera forcement sur le serveur en lui même
   API_URL = document.location.origin;
+  alert("ws not working on ngrok")
 } else {
   console.warn("Le fichier de configuration pour trouver l'API test potentiellement invalide (app-config.js)");
   protocol = document.location.href.split(":")[0]
   API_URL = `${protocol}://${document.location.hostname}`;
-  WS_URL = `${protocol == "https" ? "wss" : "ws"}://${document.location.hostname}:1337`;
+  WS_URL = `${protocol == "https" ? "wss" : "ws"}://${document.location.hostname}`;
 }
 
 module.exports = { API_URL: API_URL + "/api", WS_URL };
